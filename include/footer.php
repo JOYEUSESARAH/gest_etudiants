d83583421f5949ef9fd1802b8e4ed26141321bbc
Sarah JOYEUSE <joysarah15@gmail.com>
2023-08-24 16:06:04 +0000

    projet

---
+++ include/footer.php
@@ -0,0 +1,3 @@
+<footer class="foot bg-dark">
+    <h3 class="text-white">Copyright @ Omega 2023  </h3>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,6 @@
+
+        <div class="" id="banniere">
+            <div class="justify-content-center">
+                <img src="../../font/picture/Logo-Universite-Thomas-Sankara.png" alt="logo de gauche" class="logo">
+            </div>
+        </div>
